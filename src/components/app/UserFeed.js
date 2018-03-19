5e7580617a00e859e4094549db7a9480cdafdd80
mthorry <mthorry@gmail.com>
2018-03-19 16:33:53 -0400

    add/display likes

--- src/components/app/UserFeed.js
+++ src/components/app/UserFeed.js
@@ -36,34 +36,63 @@ class UserFeed extends React.Component {
 	getPhotoLikes = () => {
 // router.get('/api/:username/photos/:photo_id/likes', db.getPhotoLikes);
 		let {photos} = this.state
-		for (var i = 0; i < photos.length; i++) {
 		axios
-		.get(`/api/${this.props.user.username}/photos/${photos[i]['id']}/likes`)
+		.get('/api/likes')
 			.then( res => {
-				console.log(res)
-				photos[i]['likes'] = res.data.likes
+				this.setState({
+					likes: res.data.likes
+				})
 			})
-		}
+	}
+
+	addLike = (e) => {
+		// e.preventDefault()
+		// console.log(e.target.dataset, e.target.dataset.id, e.target.dataset.username)
+		axios
+		.post(`/api/${e.target.dataset.username}/photos/${e.target.dataset.id}/likes`)
+			.then( this.getPhotoLikes() )
+		.catch(err => {
+			console.log(`feed err`, err);
+		});
+	}
+
+	removeLike = (e) => {
+		console.log("UNLIKE!")
 	}
 
   render() {
     const { users, photos, likes, allUsers } = this.state;
     const { user } = this.props;
- 	console.log(photos)
+
+ 	photos.forEach( photo => {
+ 		photo.likes = []
+ 		photo.liked = false
+ 		likes.forEach( like => {
+ 			if (like.photo_id == photo.id ) {
+ 				photo.likes.push(like)
+ 			}
+ 		})
+ 		photo.likes.forEach( like =>{
+ 			if (like.username == user.username) {
+ 				photo.liked = true
+ 			}
+ 		})
+ 	})
 
     let feedPhotos = photos.map(photo => (
-          <div className="photo" key={photo.id}>
-              <h4 className="p-bold">{photo.username}</h4>
+          <div className="photo" key={photo.id} id={photo.id}>
+              <h3 className="p-bold">{photo.username}</h3>
               <p>{ `${moment(photo.date_created).fromNow()}` }</p>
             <img src={photo.url} alt={photo.caption} width="400" />
             <div className="likes-comments">
-            <p>{ } likes </p>
-              <i className="far fa-heart" />
+            <h4> {photo.likes.length} { photo.likes.length > 1 || photo.likes.length == 0 ? `borks` : `bork` } {photo.liked ? <i className="fas fa-heart"/> : `♡` }</h4>
+            	<p>{!photo.liked ? <button onClick={this.addLike} data-id={photo.id} data-username={photo.username}>Add a bork!</button> : <button onClick={this.removeLike} data-id={photo.id} data-username={photo.username}>Un-bork! 💔</button>  }</p>
             </div>
             <div className="photo-bottom">
-              <p className="p-caption">{photo.caption}</p>
+              <p className="p-caption">{photo.caption} </p>
             </div>
             <br/>
+		<hr/>
           </div>
         ))
 
